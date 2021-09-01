173bf67fed89c87f2b4381315fde260a9cb02b52
jo ladthanun <ladthanun@gmail.com>
2021-09-01 15:20:12 +0700

    blink test add

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
 #include <PubSubClient.h>
 
+#define LED_BUILTIN 2
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println(HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  Serial.println(LOW);
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000); 
 }
