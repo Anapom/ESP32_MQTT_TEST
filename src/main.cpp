b7644e7679fe7da4fa00297e6981c981455efaaf
Anapom <prachayaudom@gmail.com>
2021-09-06 10:51:38 +0700

    done?

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,131 @@
 #include <Arduino.h>
 #include <PubSubClient.h>
+#include <WiFi.h>
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "Cakehouse_2G";
+const char* password = "0992650663";
+const int mqttPort = 1884;
+const char* mqttUser = "rdd01";
+const char* mqttPassword = "rdd01";
+
+// Add your MQTT Broker IP address, example:
+//const char* mqtt_server = "192.168.1.144";
+const char* mqtt_server = "iot.mea.or.th:1884";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi();
+void callback(char* topic, byte* message, unsigned int length);
+void reconnect();
+
+float data;
 
 #define LED_BUILTIN 2
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  setup_wifi();
+  client.setServer(mqtt_server, mqttPort);
+  client.setCallback(callback);
+
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(HIGH);
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  Serial.println(LOW);
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000); 
+ if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    
+    // Data input
+    data = 50;
+    
+    // Convert the value to a char array
+    char dataString[8];
+    dtostrf(data, 1, 2, dataString);
+    Serial.print("data: ");
+    Serial.println(dataString);
+    client.publish("esp32/temperature", dataString);
+
+  } 
 }
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client",mqttUser, mqttPassword )) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
